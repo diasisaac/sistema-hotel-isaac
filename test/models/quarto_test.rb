eed4af011a83d87eed096c520a9ff8e9242488cf
Erik Alexandre Bezerra <alexandre1erik@gmail.com>
2022-04-19 21:55:24 -0300

    Teste de quarto

--- test/models/quarto_test.rb
+++ test/models/quarto_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class QuartoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "criando quarto" do
+    quarto = Quarto.new(nome: 'Apartamento de 2 Quartos', andar:'5', diaria: '100', capacidade: '5',descricao:'Cozinha privativa, Banheiro privativo, Ar-condicionado e WiFi Gratuito')
+    assert quarto.save
+  end
+  test "criando quarto sem nome" do
+    quarto = Quarto.new(nome: '', andar:'5', diaria: '100', capacidade: '5',descricao:'Cozinha privativa, Banheiro privativo, Ar-condicionado e WiFi Gratuito')
+    assert_not quarto.save
+  end
+
+  test "editando quarto" do
+    quarto = Quarto.new(nome: 'Apartamento de 2 Quartos', andar:'5', diaria: '100', capacidade: '5',descricao:'Cozinha privativa, Banheiro privativo, Ar-condicionado e WiFi Gratuito')
+    quarto.save
+    assert quarto.update(nome: 'Apartamento de 2 Quartos', andar:'8', diaria: '100', capacidade: '5',descricao:'Cozinha privativa, Banheiro privativo, Ar-condicionado e WiFi Gratuito')
+  end
+
+
 end
