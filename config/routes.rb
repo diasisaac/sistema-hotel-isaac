c8562a7a61bd262f598dc916bdc9134dd4c0829f
lrs <lucas_romeiro@outlook.com>
2022-05-13 14:03:22 -0300

    excluindo devise usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   #devise_scope :usuarios do
   #  get "/some/route" => "some_devise_controller"
   #end
-  devise_for :usuarios
 
   root to: 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
