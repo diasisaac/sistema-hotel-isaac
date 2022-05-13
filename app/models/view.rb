c41f9733a8d910904e75c092a88772e96ab93d8d
lrs <lucas_romeiro@outlook.com>
2022-05-13 14:51:58 -0300

    g devise views e adicionando styles

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,14 +1,6 @@
-class User < ApplicationRecord
+class View < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  # Roles available to each user
-  enum role: { Cliente: 0, Funcionario: 1, Admin: 2 }
-  after_initialize :set_default_role, :if => :new_record?
-
-  def set_default_role
-    self.role ||= :user
-  end
 end
