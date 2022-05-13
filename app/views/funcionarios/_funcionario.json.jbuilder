3cdfa44423d279df60b38ba790b573f31bc4d329
lrs <lucas_romeiro@outlook.com>
2022-05-13 13:59:50 -0300

    excluindo scaffold funcionarios

--- app/views/funcionarios/_funcionario.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! funcionario, :id, :nome, :cpf, :email, :cargo, :created_at, :updated_at
-json.url funcionario_url(funcionario, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   resources :reservas
   resources :quartos
-  resources :funcionarios
   root to: 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
