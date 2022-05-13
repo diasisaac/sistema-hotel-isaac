9ad7437187c5e85f96008a0c97193bbc1b52448f
lrs <lucas_romeiro@outlook.com>
2022-05-13 14:09:35 -0300

    criando controller pages home e configurando root 'pages#home'

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  #devise_for :usuarios
-  #as :usuarios do
-  #  get "sign_in", to: "devise/sessions#new"
-  #end
+  root 'pages#home'
 
-  #devise_scope :usuarios do
-  #  get "/some/route" => "some_devise_controller"
-  #end
 
-  root to: 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
