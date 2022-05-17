02169013cbb7c5e446caea916108926501cc3e9a
lrs <lucas_romeiro@outlook.com>
2022-05-16 23:31:56 -0300

    Refactoring Remove Dead Code

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,25 +2,20 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :update, :destroy, :index, :show]
   before_action :set_room, only: %i[ show edit update destroy ]
 
-  # GET /rooms or /rooms.json
   def index
     @rooms = Room.all
   end
 
-  # GET /rooms/1 or /rooms/1.json
   def show
   end
 
-  # GET /rooms/new
   def new
     @room = Room.new
   end
 
-  # GET /rooms/1/edit
   def edit
   end
 
-  # POST /rooms or /rooms.json
   def create
     @room = Room.new(room_params)
 
@@ -35,7 +30,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /rooms/1 or /rooms/1.json
   def update
     respond_to do |format|
       if @room.update(room_params)
@@ -48,7 +42,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  # DELETE /rooms/1 or /rooms/1.json
   def destroy
     @room.destroy
 
@@ -63,12 +56,10 @@ class RoomsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_room
       @room = Room.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def room_params
       params.require(:room).permit(:name, :capacity, :price, :detail)
     end
